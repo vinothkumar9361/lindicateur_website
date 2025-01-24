f9c7391bd9682c351347cdd03db5d9bacc9e5d9d
vinothkumar9361 <vinoth@akkenna.com>
2025-01-24 19:22:06 +0530

    corrections updated

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['https://lindicateurcontainer.s3.rbx.io.cloud.ovh.net', 'https://lindicateurcontainer.s3.rbx.io.cloud.ovh.net', 'lindicateurcontainer.s3.rbx.io.cloud.ovh.net'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lindicateurcontainer.s3.rbx.io.cloud.ovh.net",
+      },
+    ],
   },
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'lindicateurcontainer.s3.rbx.io.cloud.ovh.net',
-    },
-  ],
   reactStrictMode: true,
   trailingSlash: true,
 };

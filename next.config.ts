ce2ab7bbef9b9e08ba9e446612e9f4a9c6127b61
vinothkumar9361 <vinoth@akkenna.com>
2025-01-24 19:31:17 +0530

    removed

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lindicateurcontainer.s3.rbx.io.cloud.ovh.net",
-      },
-    ],
-  },
+ 
   reactStrictMode: true,
   trailingSlash: true,
 };

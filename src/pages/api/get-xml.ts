8f433d39a8732cdad180f1df2e6a43b75fc7ea58
vinothkumar9361 <vinoth@akkenna.com>
2025-01-24 22:56:18 +0530

    xml correction updated

--- src/pages/api/get-xml.ts
+++ src/pages/api/get-xml.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
-        const response = await axios.get("http://www.industrie-mag.com/backend.xml");
+        const response = await axios.get("https://www.industrie-mag.com/backend.xml");
         res.setHeader("Content-Type", "application/xml");
         res.status(200).send(response.data);
     } catch (error) {
